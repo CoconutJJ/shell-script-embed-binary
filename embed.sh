25e73fbdb7602ea651d92da3ca8e32b39dac1d75
David Yue <davidyue5819@gmail.com>
2023-02-26 13:46:24 -0800

    added more methods for handling payload

--- embed.sh
+++ embed.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
-EXE_NAME=""
 
-_exec_payload() {
-    PAYLOAD_LINE=$(grep -n "__PAYLOAD__" $0 | tail -n 1 | cut -d: -f1)
-    BINARY_LINE=$(($PAYLOAD_LINE + 1))
-    tail -n +${BINARY_LINE} $0 | base64 -d | tar -xjf - -C /tmp
-    # execute the binary, no return
-    exec /tmp/${EXE_NAME} $@
+# Modify this to your path to binary executable
+EXE_NAME="a.out"
+EXEC_PATH=""
+
+_load_payload() {
+    tar -cjf - $1 | base64 | fold >> $0
 }
 
-_exec_payload $@
+_decompress_payload() {
+    if [ -n "$EXEC_PATH" ]; then
+        return
+    fi
+
+    PAYLOAD_LINE=$(grep -n "__PAYLOAD__" $0 | tail -n 1 | cut -d: -f1)
+    BINARY_LINE=$(($PAYLOAD_LINE + 1))
+    tail -n +${BINARY_LINE} $0 | base64 -d | tar -xjf - -C $1
+    EXEC_PATH="$1/${EXE_NAME}"
+}
+
+_install_payload() {
+    _decompress_payload /tmp
+
+    mv ${EXEC_PATH} $1
+    EXEC_PATH=$1
+}
+
+_exec_payload() {
+    _install_payload "/tmp/payload"
+
+    exec ${EXEC_PATH} $@
+}
 
 # do not allow shell script to execute into the payload
 exit 1
